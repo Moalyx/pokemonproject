779c11e8fd07253aa94d537b621592d9594d9ddf
Moalyx <82666302+Moalyx@users.noreply.github.com>
2021-04-20 14:06:26 +0200

    TURN ARRAYS INTO ARRAYLISTS

--- app/src/main/java/com/example/projetpokemon/MyAdapter.java
+++ app/src/main/java/com/example/projetpokemon/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.projetpokemon;
 
 import android.content.Context;
+import android.content.pm.ShortcutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,17 +11,26 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
-    String data1[], data2[];
-    int imag[];
+    List <String> data1 = new ArrayList<>();
+    List <String> data2 = new ArrayList<>();
+    List <Integer> title = new ArrayList<>();
+    List <Integer> imag= new ArrayList<>() ;
     Context context;
 
-    public MyAdapter(Context ct, String s1[], String s2[], int images[]){
-        this.context = ct;   /*a quoi fait reference ce context je ne comprends pas*/
+    public MyAdapter(Context ct,List <String> s1,List <String> s2,List<Integer> title, List <Integer> images){
+        this.context = ct;       /*a quoi fait reference ce context je ne comprends pas*/
         this.data1 = s1;
         this.data2 = s2;
+        this.title= title;
         this.imag = images;
+
+
+
 
 
     }
@@ -36,26 +46,39 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) { /*j'aimerai des eclaircissements sur la position*/
-        holder.myText1.setText(data1[position]);
-        holder.myText2.setText(data2[position]);
-        holder.myImageView.setImageResource(imag[position]);
+        holder.myText1.setText(data1.get(position));
+        holder.myText2.setText(data2.get(position));
+        holder.imageViewTitle.setImageResource(title.get(position));
+        holder.myImageView.setImageResource(imag.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return imag.length;
+        return imag.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {    /*pourquoi on met une classe dans une autre classe?*/
 
-        TextView myText1, myText2;
+        TextView myText1;
+        TextView myText2;
+        ImageView imageViewTitle;
         ImageView myImageView;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
             myText1 = itemView.findViewById(R.id.mytext1);
             myText2 = itemView.findViewById(R.id.mytext2);
+            imageViewTitle = itemView.findViewById(R.id.imageViewTitle);
             myImageView = itemView.findViewById(R.id.myImageView);
         }
     }
+
+
+
+
+
+
+
 }
+
--- app/src/main/java/com/example/projetpokemon/MyRecyclerView.java
+++ app/src/main/java/com/example/projetpokemon/MyRecyclerView.java
@@ -1,20 +1,28 @@
 package com.example.projetpokemon;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MyRecyclerView extends AppCompatActivity {
 
 
     RecyclerView recyclerView;
 
-    String s1[], s2[];
+    List <String> s1;
 
-    int images[] = {R.drawable.canarticho_pokedex, R.drawable.dodrio_pokedex, R.drawable.flagadoss_pokedex, R.drawable.galopa_pokedex, R.drawable.lamantine_pokedex,
-            R.drawable.magneton_pokedex};
+    List <String> s2;
+
+    List <Integer> title = new ArrayList<>();
+
+    List <Integer> images = new ArrayList<>();
 
 
 
@@ -26,11 +34,24 @@ public class MyRecyclerView extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
-        s1 = getResources().getStringArray(R.array.pokemon_name); /*pourquoi array.programming_Languages et pas : string-array.programming*/
+        title.add(R.drawable.canarticho);
+        title.add(R.drawable.dodrio);
+        title.add(R.drawable.flagadoss);
+        title.add(R.drawable.galopa);
+        title.add(R.drawable.lamantine);
+        title.add(R.drawable.magneton);
 
-        s2 = getResources().getStringArray(R.array.description);
+        images.add(R.drawable.canarticho_pokedex);
+        images.add(R.drawable.dodrio_pokedex);
+        images.add(R.drawable.flagadoss_pokedex);
+        images.add(R.drawable.galopa_pokedex);
+        images.add(R.drawable.lamantine_pokedex);
+        images.add(R.drawable.magneton_pokedex);
 
-        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        s1 = Arrays.asList(getResources().getStringArray(R.array.pokemon_scream));
+        s2 = Arrays.asList(getResources().getStringArray(R.array.description));
+
+        MyAdapter myAdapter = new MyAdapter(this,s1,s2, title, images);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this)); /*qu'est ce qu'un layoutmanager*/
 
